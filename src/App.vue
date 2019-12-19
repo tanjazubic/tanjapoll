c2d820abd38dada0f5fcf624ad7e4ab748b6a550
Tanja Zubic <tanjazubic@Tanjas-MacBook-Pro.local>
2019-12-19 16:18:34 +0100

    backback

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,74 @@
 <template>
     <div>
-        <h4>Sign in / sign up: </h4>
         <amplify-authenticator></amplify-authenticator>
-        <div>
-  <div id="app" class="hello">
-<h1>Welcome to the Serverless Voting App. Now with Amplify!</h1>
-      <h4>You can vote as many times as you like. Click away!</h4>
-<b-row align-h="center" class="mt-5">
-      <b-card-group deck>
-        <b-card bg-variant="success" text-variant="white" header="Vote Yes" class="text-center" footer-tag="footer">
-          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-          <b-button size="lg" variant="primary"  @click="vote('yes')">Button</b-button>
-          <em slot="footer">{{ votesYes }} voted</em>
-        </b-card>
-<b-card bg-variant="danger" text-variant="white" header="Vote No" class="text-center" footer-tag="footer">
-          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-          <b-button size="lg" variant="primary" @click="vote('no')">Button</b-button>
-          <em slot="footer">{{ votesNo }} voted</em>
-        </b-card>
-      </b-card-group>
-    </b-row>
-    <b-row align-h="center" class="mt-5">
-      <p>Questions? Ask James <a href="https://twitter.com/jbesw">@jbesw</a>.</p>
+      </div>
+    <div id="app" class="hello">
+  <h1>Welcome to the Serverless Voting App. Now with Amplify!</h1>
+        <h4>Click to vote on this very important issue.</h4>
+        <h4>You can vote as many times as you like. Click away!</h4>
+  <b-row align-h="center" class="mt-5">
+        <b-card-group deck>
+          <b-card bg-variant="success" text-variant="white" header="Vote Yes" class="text-center" footer-tag="footer">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            <b-button size="lg" variant="primary"  @click="vote('yes')">Button</b-button>
+            <em slot="footer">{{ votesYes }} voted</em>
+          </b-card>
+  <b-card bg-variant="danger" text-variant="white" header="Vote No" class="text-center" footer-tag="footer">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            <b-button size="lg" variant="primary" @click="vote('no')">Button</b-button>
+            <em slot="footer">{{ votesNo }} voted</em>
+          </b-card>
+        </b-card-group>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <p>Questions? Ask James <a href="https://twitter.com/jbesw">@jbesw</a>.</p>
+      </b-row>
       <amplify-sign-out></amplify-sign-out>
-    </b-row>
-  </div>  
-</template>
+    </div>  
+  </template>
 
-<script>
-  import { API } from 'aws-amplify'
-  import { Auth } from 'aws-amplify'
-  import { AmplifyEventBus } from 'aws-amplify-vue'
-
-  export default {
-    name: 'app',
-    data() {
-      return {
-        apiName: 'tanjapollAPI',
-        votesYes: 0,
-        votesNo: 0
-      }
-    },
-    
-
-    methods: {
-      
-      vote: async function (vote) {
-        const init = {
-          queryStringParameters: {
-            vote
-          }
-        }
-        const response = await API.post(this.apiName, '/votes', init)
-        if (vote === 'yes') this.votesYes = response.data.Attributes.votesYes
-        if (vote === 'no') this.votesNo = response.data.Attributes.votesNo
-      },
-      updateVotes: async function () {
-        const response = await API.get(this.apiName, '/votes/poll-001')
-        this.votesNo = response[0].votesNo
-        this.votesYes = response[0].votesYes    
-      }
-    },
-    created () {
-      this.updateVotes()
-      setInterval(this.updateVotes, 99999999)
+  <script>
+import { API } from 'aws-amplify'
+export default {
+  name: 'app',
+  data() {
+    return {
+      apiName: 'pollCounterAPI',
+      votesYes: 0,
+      votesNo: 0
     }
+  },
+  methods: {
+    vote: async function (vote) {
+      const init = {
+        queryStringParameters: {
+          vote
+        }
+      }
+      const response = await API.post(this.apiName, '/votes', init)
+      if (vote === 'yes') this.votesYes = response.data.Attributes.votesYes
+      if (vote === 'no') this.votesNo = response.data.Attributes.votesNo
+    },
+    updateVotes: async function () {
+      const response = await API.get(this.apiName, '/votes/poll-001')
+      this.votesNo = response[0].votesNo
+      this.votesYes = response[0].votesYes    
+    }
+  },
+  created () {
+    this.updateVotes()
+    setInterval(this.updateVotes, 9999999999)
   }
-  </script>
+}
+</script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  </style>
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+    </style>
